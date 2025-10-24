29acd5db8255780e3a9067feab86e58c117f4108
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-10-24 18:15:09 +0100

    first commit

---
+++ src/main/java/org/yascode/interaction_plsql/InteractionPlsqlApplication.java
@@ -0,0 +1,13 @@
+package org.yascode.interaction_plsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractionPlsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractionPlsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yascode/interaction_plsql/InteractionPlsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yascode.interaction_plsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractionPlsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
